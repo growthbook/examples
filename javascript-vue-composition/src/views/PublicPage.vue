18c16f7afd12f4b31fd0f810cf681c67aca2a892
tinahollygb <tina@growthbook.io>
2022-11-11 12:09:33 -0800

    readme; page scaffolding

---
+++ javascript-vue-composition/src/views/PublicPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    <h1>This is a Public page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
