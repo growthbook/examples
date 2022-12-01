7a99e885abe22d837b2840503e28a3e1b8b9128b
tinahollygb <tina@growthbook.io>
2022-11-30 16:23:14 -0800

    rm old comments

--- webhooks-impl/events/events.router.js
+++ webhooks-impl/events/events.router.js
@@ -10,15 +10,13 @@ router.use(bodyParser.raw({
   type: 'application/json'
 }))
 // 2. verify web hook signature
-// 🚧 Not yet implemented
 router.use(authenticateWebHooks({ secret: process.env.GROWTHBOOK_EVENTS_WEBHOOKS_SECRET }))
 
-// 🚧 Not yet implemented
 router.post('/webhooks', (req, res) => {
   console.log('Receiving request', req.body)
 
   // Respond first
-  res.json({ status: 'ok'})
+  res.json({ status: 'all is good'})
 
   // Handle event
 })
