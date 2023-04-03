818def1fb21db74acc09a303c6b9a5fd7dac3bd1
bttf <2374625+bttf@users.noreply.github.com>
2023-04-03 14:43:36 -0400

    Refine preview linking

--- next-js/components/VisualExperimentsDisplay.tsx
+++ next-js/components/VisualExperimentsDisplay.tsx
@@ -1,4 +1,12 @@
+import { isURLTargeted } from "@growthbook/growthbook";
 import { useGrowthBook } from "@growthbook/growthbook-react";
+import qs from "qs";
+
+const genPreviewLink = ({ name, i }: { name: string; i: number }) => {
+  const href = window.location.href;
+  const [root, params] = href.split("?");
+  return `${root}?${qs.stringify({ [name]: i })}`;
+};
 
 export default function VisualExperimentsDisplay() {
   const gb = useGrowthBook();
@@ -17,46 +25,56 @@ export default function VisualExperimentsDisplay() {
             {e.name} ({e.status})
             <ul>
               <li>
-                Target URLs
+                URL Targets
                 <ul>
                   {e?.urlPatterns?.map((p, i) => (
                     <li key={i}>
                       <table border={1} cellPadding={4}>
                         <thead>
-                          <th>Type</th>
-                          <th>Pattern</th>
-                          <th>Include?</th>
+                          <tr>
+                            <th>Type</th>
+                            <th>Pattern</th>
+                            <th>Include?</th>
+                          </tr>
                         </thead>
                         <tbody>
-                          <td>{p.type}</td>
-                          <td>{p.pattern}</td>
-                          <td>{p.include ? "true" : "false"}</td>
+                          <tr>
+                            <td>{p.type}</td>
+                            <td>{p.pattern}</td>
+                            <td>{p.include ? "true" : "false"}</td>
+                          </tr>
                         </tbody>
                       </table>
                     </li>
                   ))}
                 </ul>
               </li>
-              <li>
-                Preview links
-                <ul>
-                  {e.variations.map((_v, i) => (
-                    <li key={i}>
-                      <a
-                        href={`${window.location.href}?${e.name}=${i}`}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        {`${window.location.href}?${e.name}=${i}`}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </li>
+
+              {isURLTargeted(window.location.href, e.urlPatterns ?? []) && (
+                <li>
+                  Preview links
+                  <ul>
+                    {e.variations.map(
+                      (_v, i) =>
+                        e.name && (
+                          <li key={i}>
+                            <a href={genPreviewLink({ name: e.name, i })}>
+                              {genPreviewLink({ name: e.name, i })}
+                            </a>
+                          </li>
+                        )
+                    )}
+                  </ul>
+                </li>
+              )}
             </ul>
           </li>
         ))}
       </ul>
+      <p>
+        To see preview links, add URL targets to your experiment that target the
+        current URL ({window.location.href.split("?")[0]}).
+      </p>
     </>
   );
 }
